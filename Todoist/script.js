d0926cca3211302a1a0d4b705c47efa182987b82
Prajakta Patil <88300998+PrajaktaPatil05@users.noreply.github.com>
2024-01-17 13:54:19 +0530

    Update script.js

--- Todoist/script.js
+++ Todoist/script.js
@@ -3,19 +3,19 @@ function addTask() {
     var taskList = document.getElementById("task-list");
 
     if (taskInput.value.trim() !== "") {
-        // Create a new task item
+        
         var li = document.createElement("li");
         li.innerHTML = taskInput.value + '<button onclick="removeTask(this)">Remove</button>';
         
-        // Append the new task item to the task list
+        
         taskList.appendChild(li);
 
-        // Clear the input field
+        
         taskInput.value = "";
     }
 }
 
-// Function to remove a task
+
 function removeTask(button) {
     var li = button.parentNode;
     var taskList = document.getElementById("task-list");
